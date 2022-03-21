48b255d10f68605ca3d2cb479161a00a67470f31
htnosm <htnosm@gmail.com>
2022-03-21 16:58:37 +0900

    fix list_bucket_call_count

--- trigger.tf
+++ trigger.tf
@@ -1,7 +1,12 @@
 resource "null_resource" "list_bucket_call_count" {
   count = var.trigger_list_bucket_call_count ? 1 : 0
 
+  triggers = {
+    enabled = timestamp()
+  }
+
+  # s3:ListBuckets
   provisioner "local-exec" {
-    command = "aws s3 ls s3://${aws_s3_bucket.example.id}"
+    command = "aws s3 ls"
   }
 }
